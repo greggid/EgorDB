34dd9a95ab420087015b77c00e313177eaeb86ca
greggit <roshchinlykav@mail.ru>
2022-12-19 13:38:07 +0300

    New try add connection to bd

--- server/lib/credentials.py
+++ server/lib/credentials.py
@@ -6,9 +6,9 @@ router = APIRouter()
 
 
 def credentialsExist(data: dict) -> bool:
-    username = data.get("username", None)
-    password = data.get("password", None)
-    if not username or not password:
+    uname = data.get("username", None)
+    upass = data.get("password", None)
+    if not uname or not upass:
         return False
     return True
 
@@ -19,3 +19,8 @@ async def login(request: Request) -> dict:
     if credentialsExist(data):
         return data
     return False
+
+def separateData(login):
+    string_uname = data["uname"]
+    string_pass = data["pass"]
+    return string_uname, string_pass
--- server/lib/db.py
+++ server/lib/db.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 import mysql.connector
-from lib import credentials
+from . import credentials
 from . import settings
+from fastapi import APIRouter
+
+router = APIRouter()
+
 
 def connectToDB():
     try:
         __mydb = mysql.connector.connect(
-        host="localhost",
-        user="egordb",
-        password="egordb_password",
-        database ="egordb"
-
+            host="localhost",
+            user="egordb",
+            password="egordb_password",
+            database="egordb",
         )
+        print("Connection to MySQL DB successful")
         return __mydb
     except Exception as e:
-        raise SystemExit(e)
+        print("Connection to MySQL DB unsuccessful")
 
 
 __mydb = connectToDB()
 
 
-def sqlRequest(login):
-    mycursor = __mydb.cursor()
-    mycursor.execute("SELECT * FROM users")
-    myresult = mycursor.fetchone()
-    return myresult
+@router.post("/server/login")
+async def sqlRequest(separateData, __mydb):
+    try:
+        mycursor = await __mydb.cursor()
+        mycursor.execute("SELECT * FROM users WHERE username = string_uname")
+        row = mycursor.fetchall()
+        return row
+    except Exception as e:
+        raise SystemExit(e)
+
+
+# string_uname, string_pass
