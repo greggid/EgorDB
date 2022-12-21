73df3c4613d325f88a52b43c5d87ccac424b1b48
greggit <roshchinlykav@mail.ru>
2022-12-22 02:13:30 +0300

    login is working

--- server/lib/credentials.py
+++ server/lib/credentials.py
@@ -18,14 +18,17 @@ def cookieExist(cookie: str) -> bool:
 @router.post("/server/login")
 async def login(request: Request):
     incorrect = {"succesful": False, "reason": "Incorrect login or password"}
-    missing = {"succesful": False, "reason": "Missing data"}
+    missing_uname = {"succesful": False, "reason": "Enter login"}
+    missing_upass = {"succesful": False, "reason": "Enter password"}
     correct = {"succesful": True}
     cookie = request.cookies.get("egoSession")
     data = await request.json()
     uname = data.get("username", None)
     upass = data.get("password", None)
-    if not uname or not upass:
-        return JSONResponse(status_code=401, content = missing)    
+    if not uname:
+        return JSONResponse(status_code=403, content=missing_uname)
+    if not upass:
+        return JSONResponse(status_code=403, content=missing_upass)
     if not db.login(uname, upass, cookie):
-        return JSONResponse(status_code=402, content = incorrect)
-    return JSONResponse(status_code=200, content = correct)
+        return JSONResponse(status_code=401, content=incorrect)
+    return JSONResponse(status_code=200, content=correct)
